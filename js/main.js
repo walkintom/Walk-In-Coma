ba54823685722a98c38b11f259fa3af80047fa75
walkintom <tom.case@icloud.com>
2018-03-22 10:05:43 +0000

    JS Tweaks

--- js/main.js
+++ js/main.js
@@ -93,12 +93,13 @@
 
 })(jQuery);
 
+//$(function() {
+//    $('section.about a').click(function() {
+//        $('html, body').animate({scrollTop: $(document).height()}, 2000);
+//        return false;
+//    });
+//});
+
 $(function() {
     $('footer').footerReveal();
-});
-$(function() {
-    $('section.about a').click(function() {
-        $('html, body').animate({scrollTop: $(document).height()}, 2000);
-        return false;
-    });
 });
