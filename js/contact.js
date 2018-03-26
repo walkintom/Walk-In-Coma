2207db69032c75382c4cf9ca24a50f460ec60302
walkintom <tom.case@icloud.com>
2018-03-26 15:01:25 +0100

    JS Tweaks

---
+++ js/contact.js
@@ -0,0 +1,22 @@
+// Contact Form
+
+$(document).ready(function(){
+    $("#contactForm").submit(function(event){
+      event.preventDefault();
+      $.ajax({
+        url: "https://www.enformed.io/4slmkf4z",
+        method: "post",
+        dataType: "json",
+        accepts: "application/json",
+        data: $("#contactForm").serialize(),
+        success: function(){
+          console.log("Your form was successfully received!");
+          // Show a success message here...
+        },
+        error: function(){
+          console.log("Failure. Try again.");
+          // Show an error message here...
+        }
+      });
+    });
+  });
