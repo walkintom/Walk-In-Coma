d7639e7a71d11cc11bfb16d44ff29255d1332ecb
Tom Case <tom.case@icloud.com>
2022-03-14 20:09:42 +0000

    Scrollspy refinements

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@ function playPause() {
 // ScrollSpy & Smooth Scrolling
 $(document).ready(function(){
     // Add scrollspy to <body>
-    $('body').scrollspy({target: ".navbar", offset: 72});   
+    $('body').scrollspy({target: ".navbar", offset: 75});   
   
     // Add smooth scrolling on all links inside the navbar
     $('a[href*="#"]').on('click', function(event) {
@@ -40,7 +40,7 @@ $(document).ready(function(){
         // Using jQuery's animate() method to add smooth page scroll
         // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
         $('html, body').animate({
-          scrollTop: $(hash).offset().top - 72
+          scrollTop: $(hash).offset().top - 71
         }, 1100, function(){
      
           // Add hash (#) to URL when done scrolling (default click behavior)
